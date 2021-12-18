b98e115db421972d39e521b6c2b3675e064dd3f7
YuliaTsveleva <tsveleva.yulia@gmail.com>
2021-12-19 00:31:31 +0200

    refactoring Modal to hooks

--- src/Components/Modal/Modal.js
+++ src/Components/Modal/Modal.js
@@ -1,44 +1,40 @@
-import React, { Component } from 'react';
+import { useEffect } from 'react';
+import PropTypes from 'prop-types';
 import { createPortal } from 'react-dom';
 import s from './Modal.module.css';
 
 const modalRoot = document.querySelector('#modal-root');
-class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleKeyDown);
-  }
 
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleKeyDown);
-  }
+export default function Modal({ toClose, src, alt, isShow }) {
+  useEffect(() => {
+    const handleKeyDown = e => {
+      if (e.code === 'Escape' && isShow === true) toClose();
+    };
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [toClose, isShow]);
 
-  handleKeyDown = e => {
-    if (e.code === 'Escape' && this.props.isShow === true) {
-      this.props.toClose();
-    }
-  };
-
-  handleCloseByBackdrop = e => {
+  const handleCloseByBackdrop = e => {
     if (e.target === e.currentTarget) {
-      this.props.toClose();
+      toClose();
     }
   };
 
-  render() {
-    const { src, alt } = this.props;
-    return createPortal(
-      <div
-        className={s.Overlay}
-        onClick={this.handleCloseByBackdrop}
-        onKeyDown={this.handleKeyDown}
-      >
-        <div className={s.Modal}>
-          <img src={src} alt={alt} />
-        </div>
-      </div>,
-      modalRoot,
-    );
-  }
+  return createPortal(
+    <div className={s.Overlay} onClick={handleCloseByBackdrop}>
+      <div className={s.Modal}>
+        <img src={src} alt={alt} />
+      </div>
+    </div>,
+    modalRoot,
+  );
 }
 
-export default Modal;
+Modal.propTypes = {
+  toClose: PropTypes.func.isRequired,
+  src: PropTypes.string,
+  alt: PropTypes.string,
+  isShow: PropTypes.bool.isRequired,
+};
